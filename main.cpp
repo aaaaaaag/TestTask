2ba87cd4d9cce93a4235792c0424a614e16a1a2e
bdv19u133 <orto.fosfat@yandex.ru>
2020-07-13 19:27:43 +0300

    add new class Person for showin my skills in inheritance and update .gitignore

--- main.cpp
+++ main.cpp
@@ -3,15 +3,32 @@
 using std::string;
 using std::cout;
 using std::endl;
+using std::cin;
 
-class Emploee
+class Person
+{
+public:
+    Person(string inName, int inAge): name(inName), age(inAge)
+    {
+    }
+    virtual void displayInfo()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+protected:
+    string name;
+    int age;
+};
+
+class Emploee:public Person
 {
 public:
     Emploee(string inName, int inAge, string inPosition, int inSalary):
-        name(inName), age(inAge), position(inPosition), salary(inSalary)
+        Person(inName, inAge), position(inPosition), salary(inSalary)
     {
     }
-    void displayInfo()
+    void displayInfo() override
     {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
@@ -19,15 +36,32 @@ public:
         cout << "Salary: " << salary << endl;
     }
 private:
-    string name;
-    int age;
     string position;
     int salary;
 };
 
 int main()
 {
-    Emploee Denis("Denis", 19, "Junior developer", 35000);
-    Denis.displayInfo();
+    Emploee work("Denis", 19, "programmer", 35000);
+    work.displayInfo();
+    cout << "Welcome to BatExcel" << endl;
+    int inputPoint = 1;
+    while (inputPoint)
+    {
+        cout << "____________________________" << endl;
+        cout << "1 - add position" << endl;
+        cout << "2 - delete position" << endl;
+        cout << "3 - show all positions" << endl;
+        cout << "4 - find position" << endl;
+        cout << "5 - exit BatExcel" << endl;
+        cout << "Input number of your choice: ";
+        cin >> inputPoint;
+        if (inputPoint == 1)
+        {
+
+        }
+        else if (inputPoint == 5)
+            break;
+    }
     return 0;
 }
